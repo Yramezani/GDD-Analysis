5909efa8dc7f5a040d6076542c0b5c9dbb256705
Aida Ghayour'git config --global user.email aghayourkhia@mun.cagit config --global user.name Aida <aghayourkhia@mun.ca>
2018-06-01 14:41:27 +0000

    getdata.bash, gets station ID, and puts a 4 column csv file named tmp.csv, which contains max,min,mean and dates

--- getdata.sh
+++ getdata.sh
@@ -1,2 +1,7 @@
+mkdir bashout
+cd bashout
+mkdir data
 for year in `seq 1998 2008`;do for month in `seq 1 12`;do wget --content-disposition "http://climate.weather.gc.ca/climate_data/bulk_data_e.html?format=csv&stationID=$1&Year=${year}&Month=${month}&Day=14&timeframe=2&submit= Download+Data" ;done;done
+for f in *.csv; do sed '1,25d' "$f"; done | cut -d, -f1,6,8,10 >> ./data/tmp.csv
+
 
