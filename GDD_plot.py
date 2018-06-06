35193d5bfa9d36c326c2493d77c531e3751ab8c7
Tarun5678 <Tharun5678@gmail.com>
2018-06-05 21:50:23 -0230

    Update GDD_plot.py

--- GDD_plot.py
+++ GDD_plot.py
@@ -1,18 +1,20 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
-
+import numpy as np
 df=pd.read_csv("GDD.csv",usecols=['Date/Time','GDD'],parse_dates=['Date/Time'])
 df.set_index('Date/Time',inplace=True)
-fig,ax = plt.subplots(figsize=(9,7))
-ax.plot(df['GDD'],color="green")
+fig,ax = plt.subplots(figsize=(7,5))
+Cumulative_GDD = np.cumsum( df['GDD'])
+ax.plot(Cumulative_GDD,color="green")
+ax.legend(loc='upper right')
 plt.xlabel('Year')
-plt.ylabel('cumulative growing degree days(>50°F)')
-plt.title("2017-Growing Degree days comparision(>50°F)")
+plt.ylabel('cumulative growing degree days(>50Â°F)')
+plt.title("2017-Growing Degree days comparision(>50Â°F)")
 ax.yaxis.grid(True)
-plt.axes([.65, .65, .2, .2])
-plt.xticks(())
-plt.yticks(())
-plt.text(.5, .5, 'Cumulative growing degree days', ha='center', va='center',
-        size=7,color='black')
+ax.annotate("Cumulative Growing degree days\n"
+                 "as of January 2017\n"
+                ,(0.25, 0.8),
+                 xycoords="axes fraction", va="center", ha="center",
+                 bbox=dict(boxstyle="square, pad=1", fc="w"))
 plt.show()
