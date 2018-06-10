98647e9d4b01c22867fdced10e25599930b7f7df
Tarun5678 <Tharun5678@gmail.com>
2018-06-10 20:03:27 -0230

    Update GDD_plot.py

--- GDD_plot.py
+++ GDD_plot.py
@@ -2,22 +2,29 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import numpy as np
-Data=pd.read_csv("GDD.csv",usecols=['Date/Time','GDD'],parse_dates=['Date/Time'])
-df=pd.DataFrame(Data)
-cleandata=clean_data(df)
-df.to_csv('GDD.csv')
-df.set_index('Date/Time',inplace=True)
-fig,ax = plt.subplots(figsize=(7,5))
+import glob
+import os
+import sys
+import datetime
+path = "/home/jovyan/lectures/Data/*.csv"
+myfiles=[]
+for fname in glob.glob(path):#finds the GDD file in the specified path
+    Data=pd.read_csv(fname,usecols=['Date/Time','GDD'],parse_dates=['Date/Time'])
+    df=pd.DataFrame(Data)
+    cleandata=clean_data(df)#cleans the obtained raw data
+    df.to_csv('GDD.csv')
 Cumulative_GDD = np.cumsum( df['GDD'])
-ax.plot(Cumulative_GDD,color="green")
-ax.legend(loc='upper right')
+x=df['Date/Time']
+plt.plot(x,Cumulative_GDD,color="green")
+plt.legend(loc='upper right')
 plt.xlabel('Year')
 plt.ylabel('cumulative growing degree days(>50°F)')
-plt.title("2017-Growing Degree days comparision(>50°F)")
-ax.yaxis.grid(True)
-ax.annotate("Cumulative Growing degree days\n"
+plt.title("(st.johns)2017-Growing Degree days comparision(>50°F)")
+plt.grid(True)
+plt.annotate("Cumulative Growing degree days\n"
                  "as of January 2017\n"
-                ,(0.25, 0.8),
+                ,(0.3, 0.8),
                  xycoords="axes fraction", va="center", ha="center",
                  bbox=dict(boxstyle="square, pad=1", fc="w"))
 plt.show()
+
