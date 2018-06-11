3a8b1af89067e7139c65dc2db27cb3ad0ee586b4
Aida Ghayour <aghayourkhia@mun.ca>
2018-06-11 15:55:09 -0230

    Task1.py updated
    Percentile calculation corrected. minor changes in labels.

--- Task1.py
+++ Task1.py
@@ -5,7 +5,8 @@ import glob
 
 Sum = [np.nan] * 365
 average = []
-for fname in glob.glob("*GDD.csv"):  # For loop for .csv files in current folder
+T = []  # Will keep a list of GDDs for  calculating percentile
+for fname in glob.glob("*GDD.csv"):  # For loop for .csv files in given input folder
     D = pd.read_csv(fname, header=0)  # skipped rows will change if data frame's shape change
     df = pd.DataFrame(D)
     print("Accessing headers of " + fname)
@@ -13,28 +14,27 @@ for fname in glob.glob("*GDD.csv"):  # For loop for .csv files in current folder
     year = list(df['Year'])[1]
     df = df[df["Date/Time"] != str(year) + "-02-29"]  # Deletes Febuary 29th from leap year's data
     t = list(df["GDD"])
-    a = np.array([t, Sum])
-    average = np.nanmean(a, axis=0)
-    Sum += average
+    T.append(t)
+    average = np.nanmean(np.array(T), axis=0)
 
 x = df["Date/Time"]
 year = list(df['Year'])[1]
 plt.plot(x, average, color="red", label="Average")
 plt.scatter(x, t, label=str(year))
 
-U = np.array(average) * 1.05
-D = np.array(average) * 0.95
-plt.fill_between(x, U, D, alpha=0.3, color='blue', label="5-95 percentile")
+U = np.nanpercentile(T, 95, axis=0)
+D = np.nanpercentile(T, 5, axis=0)
+Uu = np.nanpercentile(T, 75, axis=0)
+Dd = np.nanpercentile(T, 25, axis=0)
 
-Uu = np.array(average) * 1.25
-Dd = np.array(average) * 0.75
-plt.fill_between(x, Uu, Dd, alpha=0.3, color='red', label="25-75 percentile")
+plt.fill_between(x, U, D, alpha=0.15, color='blue', label="5-95 percentile")
+plt.fill_between(x, Uu, Dd, alpha=0.15, color='red', label="25-75 percentile")
 
 plt.xlabel('time')
 plt.ylabel('Daily Accumulation (Celicius)')
 plt.title('Daily Growing degree days')
 plt.grid(True)
 plt.tight_layout()
-
+plt.legend()
 plt.xticks([0, 30, 58, 89, 119, 150, 180, 211, 242, 272, 303, 333],
-           ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'])
+           ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'], rotation='vertical')
