b73d1abc3abd8f779683f624ca513a46ba6fa6ab
Yousef Ramezani <yramezani@mun.ca>
2018-06-12 19:22:21 -0230

    updated download file: input(Ncity, year):output: ../Namecity,year.csv

--- Download.py
+++ Download.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[ ]:
+# In[2]:
 
 
 import os
@@ -17,10 +17,10 @@ import pandas as pd
 Return:
     string: this is a string value that defines the *.csv file that was downloaded/stored.
 """
-def download(stationid, year):
+def download(Namecity, year):
     print("Started downloading ...")
     print("Data for station with id {} for year {}".format(stationid, year))
-
+    stationid= Ncity_to_stationID(Namecity, year) 
     fname = "{}_{}_t.csv".format(stationid, year)
     url = "http://climate.weather.gc.ca/climate_data/bulk_data_e.html?format=csv&stationID="+str(stationid)+"&Year="+str(year)+"&Month=8&Day=1&timeframe=2&submit=Download+Data"
 
@@ -39,12 +39,11 @@ def download(stationid, year):
 
     data_frame = pd.read_csv(fname, skiprows=25, sep=",", encoding="ISO-8859-1")
     columns =range (0,10)
-    df = data_frame.iloc[columns]
-    df_rename = df.columns=["Date/Time","Year","Month","Day","Data Quality","Max Temp (°C)","Max Temp Flag","Min Temp (°C)","Min Temp Flag","Mean Temp (°C)"]
-    new_fname =  os.path.dirname(os.path.realpath(__file__)) + "/../csv_data/{}_{}.csv".format(stationid, year)
+    df = data_frame.iloc[:,columns]
+    new_fname =  os.path.dirname(os.path.realpath(__file__)) + "/../{}_{}.csv".format(Namecity, year)
     df_rename.to_csv(new_fname)
     print("File saved into: " + new_fname)
-
+     #data.to_csv("./Input/"+str(year)+"_"+city+"_temp.csv")
     # removing temporary saved file
     os.remove(fname)
     #print(df)
