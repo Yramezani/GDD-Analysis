17740aa6853e672956388c758843fd59f13a120b
Stephanie Gallant <smg561@mun.ca>
2018-06-13 21:15:32 -0230

    Updated function

--- GDD_bokeh.py
+++ GDD_bokeh.py
@@ -1,10 +1,10 @@
 
 # coding: utf-8
 
-# In[25]:
+# In[63]:
 
 
-from bokeh.plotting import figure, output_file, show
+from bokeh.plotting import figure, output_file, show, save
 from bokeh.models import HoverTool, BoxSelectTool
 import pandas as pd
 import numpy as np
@@ -13,20 +13,32 @@ import sys
 import glob
 
 
-# In[32]:
+# In[96]:
 
 
-def make_bokehplot(filename):
-    for filename in glob.glob("*GDD.csv"):
+""" this function takes GDD values from a .csv file and creates a bokeh plot of the cumulative GDD value through the year"""
+
+def make_bokehplot(filename): #input CSV file with GDD values
+    for filename in glob.glob("*GDD.csv"): 
         D = pd.read_csv(filename)
         df = pd.DataFrame(D)
         df.index
-        accum_GDD = np.cumsum(df['GDD'])
+        accum_GDD = np.cumsum(df['GDD']) #sums the daily GDD values to make them accumulated
 
+        #### Uncomment 3 lines below for accurate title from dataframes #####
+    
+#         fig = figure(tools="pan,box_zoom,wheel_zoom,reset,save,hover,box_select", plot_width = 700, plot_height = 700, 
+#            title = "Accumulative GDD for "  + str(list(df["Name"])[1]) +  " in "  + str(list(df["Year"])[1]), 
+#                 x_axis_label = 'Year (in days)', y_axis_label = 'Calculated GDD')
+        
+        #### Comment 3 lines below if uncommenting above #####
+    
         fig = figure(tools="pan,box_zoom,wheel_zoom,reset,save,hover,box_select", plot_width = 700, plot_height = 700, 
-           title = "Accumulative GDD for St. John's in 2017", x_axis_label = 'Year (in days)', 
-             y_axis_label = 'Calculated GDD')
+           title = "Accumulated GDD for CITY in YEAR", 
+                x_axis_label = 'Year (in days)', y_axis_label = 'Calculated GDD')
 
+        ####
+        
         hover = HoverTool(
             tooltips=[
                 ("Day of year", "$index"), 
@@ -34,21 +46,16 @@ def make_bokehplot(filename):
                     ]
                 )
 
-        fig.circle(df.index, accum_GDD, size=9, color='purple', alpha=0.5)
-        show(fig)
+        fig.circle(df.index, accum_GDD, size=9, color='green', alpha=0.5)
+        
         # save to html...
         
         basename = os.path.basename(filename) # mydir/12345.csv -> 12345
         output = basename + '_bokeh.html'
-        output_file(output, title="Accumulative GDD data for St. John's in 2017")
-    
-
-
-# In[33]:
-
-
-if __name__=="__main__":
-    filename = sys.argv[1]
-    city_name = sys.argv[2]
-    make_bokehplot(filename)
+        
+        ### absolutely could not get the output to save in another folder, arugments needed commented out below
+        
+        output_file(output, title="Accum. GDD") # mode='relative(-dev)', root_dir= notebooks/CMSC/Project/bokeh_plot
+        
+        show(fig)
 
