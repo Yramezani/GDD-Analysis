f7de23e71d790a65ad286f1939734d796ec8ff3e
Aida Ghayour <aghayourkhia@mun.ca>
2018-06-13 20:11:08 -0230

    Update DownloadData.py

--- DownloadData.py
+++ DownloadData.py
@@ -125,7 +125,7 @@ def download(Namecity, year, inputfolder="input"):
     print("File saved.")
 
 cityname=['Victoria','Montreal','Ottawa']
-years=[2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017]
+years=[2007,2008,2009,2010,2011,2012,2013,2014,2015,2016]
 for i in cityname:
     for j in years:
         download(i, j, inputfolder="input")
