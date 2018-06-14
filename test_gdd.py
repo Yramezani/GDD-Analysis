63325f64529b28b0b9613c4031467960168fa24a
Aida Ghayour <aghayourkhia@mun.ca>
2018-06-14 00:09:07 -0230

    Update and rename calculatortest.py to test_gdd.py

--- test_gdd.py
+++ test_gdd.py
@@ -1,30 +1,31 @@
 ''' Tests GDD calculator by passing variant inputs'''
 from numpy.testing import assert_allclose
+import numpy as np
 from GDDcalculate import *
 
 
 def test_in_range():
     ''' Tests if GDDcalculate.py works with temperatures between upper and lower '''
     obs = GDDcalculate([15, 16, 18, 19], [23, 24, 26, 27], 14, 28, 4)
-    exp = ([4, 6, 8, 10])
+    exp = [5.0, 6.0, 8.0, 9.0]
     assert_allclose(obs, exp, err_msg='GDD calculation Error',  rtol=1e-5, atol=0)
 
 
 def test_out_range():
     ''' Tests if GDDcalculate.py works with temperatures out of upper and lower's range '''
     obs = GDDcalculate([15, 16, 18, 19], [23, 24, 26, 27], 20, 22, 4)
-    exp = (1.0, 1.0, 1.0, 1.0)
+    exp = [1.0, 1.0, 1.0, 1.0]
     assert_allclose(obs, exp, err_msg='GDD calculation Error', rtol=1e-5, atol=0)
 
 
 def test_overlap():
     ''' Tests if GDDcalculate.py works with mintemp bigger than tbase and maxtemp smaller than tupper '''
     obs = GDDcalculate([15, 16, 18, 19], [23, 24, 26, 27], 14, 22, 4)
-    exp = ([4.5, 5, 6, 6.5])
+    exp = [4.5, 5.0, 6.0, 6.5]
     assert_allclose(obs, exp, err_msg='GDD calculation Error', rtol=1e-5, atol=0)
 
 def test_length():
     ''' Tests if GDDcalculate.py works with mismatching GDD length '''
-    obs = GDDcalculate([15, 16, 18, 19], [23, 24, 26, 27], 14, 28, 4)
-    exp = ([4, 6, 8, None])
+    obs = GDDcalculate([15, 16, 18, 19], [23, 24, 26, 27], 14, 28, 3)
+    exp = [5.0, 6.0, 8.0, np.nan]
     assert_allclose(obs, exp, err_msg='GDD calculation Error', rtol=1e-5, atol=0)
