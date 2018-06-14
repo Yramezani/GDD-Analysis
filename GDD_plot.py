fbceecbaf478f8da30e8f8d8d29577055ec08f5f
Aida Ghayour <aghayourkhia@mun.ca>
2018-06-14 09:01:35 -0230

    Update:addes docstring,changed output folder

--- GDD_plot.py
+++ GDD_plot.py
@@ -7,14 +7,13 @@ import os
 import sys
 import datetime
 
-################################This script will plot the cumulative Growing degree days for selected cities#######################
+'''This script will plot the cumulative Growing degree days for selected cities'''
 
 Calculated_GDD=[] 
-tbase = 10
-tupper = 50
 avg=[]
-#The function takes city name as input and calcultes Growing degree days and plots with time.
+
 def plots(cityname):
+    '''The function takes city name as input and calcultes Growing degree days and plots with time'''
     years=[2014,2015,2016]
     for year in years:
         for fname in glob.glob('./input/'+str(cityname) + '_' + str(year) + '.csv'):#searches for the specific file in the input folder
@@ -23,13 +22,11 @@ def plots(cityname):
             df=pd.DataFrame(Data)
             year = list(df['Year'])[1]
             df = df[df["Date/Time"] != str(year)+"-02-29"]
-            tempmax = df['Max Temp (Â°C)']
-            tempmin = df['Min Temp (Â°C)'] 
-            length = len(pd.Series.dropna(tempmin))
-            #calculates the growing degree days based on the following input
-            t= GDDcalculate(list(tempmin),list(tempmax), tbase, tupper, length)
+            tempmax = df['Max Temp (°C)']
+            tempmin = df['Min Temp (°C)'] 
+            year = list(df['Year'])[1]
+            t = list(df["GDD"])
             Calculated_GDD.append(t) 
-            #calculates the cumulative growing degree days
             Cumulative_GDD=np.cumsum(np.array(Calculated_GDD)) 
             Time=np.linspace(1,11,len(Cumulative_GDD))
             plt.plot(Time,Cumulative_GDD,label=year,linewidth = 2)
@@ -38,7 +35,7 @@ def plots(cityname):
     ax=plt.axes()
     plt.xlabel('Year')
     plt.ylabel('cumulative growing degree days(>50°F)')
-    plt.legend(loc='upper right')
+    plt.legend(loc='best')
     title='Accumulated Growing Degree Days For - ' + str(cityname)
     plt.title(title,color="black", fontsize=16)
     plt.annotate("Cumulative Growing degree days\n"+
@@ -50,11 +47,11 @@ def plots(cityname):
     plt.xticks(np.arange(12),('1-Jan', '1-Feb', '1-Mar', '1-Apr', '1-May', '1-Jun', '1-Jul', '1-Aug', '1-Sep', '1-Oct', '1-Nov', '1-Dec'))
     ax.yaxis.grid(True)        
 plots('Victoria')
-plt.savefig('output/Victoria.png')
+plt.savefig('./docs/VictoriaGDD.png')
 plt.show()
 plots('Montreal')
-plt.savefig('output/Montreal.png')
+plt.savefig('./docs/MontrealGDD.png')
 plt.show()
 plots('Toronto')
-plt.savefig('output/Toronto.png')
+plt.savefig('./docs/TorontoGDD.png')
 plt.show()
