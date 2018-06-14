8c6f05c1b60a431adf7136766d893476a9a5836f
Tarun5678 <Tharun5678@gmail.com>
2018-06-14 04:55:07 -0230

    Update GDD_plot.py

--- GDD_plot.py
+++ GDD_plot.py
@@ -12,6 +12,7 @@ import datetime
 Calculated_GDD=[] 
 tbase = 10
 tupper = 50
+avg=[]
 #The function takes city name as input and calcultes Growing degree days and plots with time.
 def plots(cityname):
     years=[2014,2015,2016]
@@ -31,16 +32,23 @@ def plots(cityname):
             #calculates the cumulative growing degree days
             Cumulative_GDD=np.cumsum(np.array(Calculated_GDD)) 
             Time=np.linspace(1,11,len(Cumulative_GDD))
-            plt.plot(Time,Cumulative_GDD,label=year)
-        ax=plt.axes()
-        plt.xlabel('Year')
-        plt.ylabel('cumulative growing degree days(>50°F)')
-        plt.legend(loc='upper right')
-        title='Accumulated Growing Degree Days For - ' + str(cityname)
-        plt.title(title,color="black", fontsize=16)
-        plt.xticks(np.arange(12),('1-Jan', '1-Feb', '1-Mar', '1-Apr', '1-May', '1-Jun', '1-Jul', '1-Aug', '1-Sep', '1-Oct', '1-Nov', '1-Dec'))
-        ax.yaxis.grid(True)
-        
+            plt.plot(Time,Cumulative_GDD,label=year,linewidth = 2)
+        Cumu_GDD=np.array(Cumulative_GDD)
+        avg.append(Cumu_GDD[len(Cumu_GDD)-1])
+    ax=plt.axes()
+    plt.xlabel('Year')
+    plt.ylabel('cumulative growing degree days(>50°F)')
+    plt.legend(loc='upper right')
+    title='Accumulated Growing Degree Days For - ' + str(cityname)
+    plt.title(title,color="black", fontsize=16)
+    plt.annotate("Cumulative Growing degree days\n"+
+               "as of 2016\n"+"2014 -2015 Average = "+str(avg[0])+"\n"
+                 "as of 2016\n"+"2015 -2016 Average = "+str(avg[1])+"\n"
+                ,(0.45, 0.8),
+                 xycoords="axes fraction", va="center", ha="center",
+                 bbox=dict(boxstyle="square, pad=1", fc="w"))
+    plt.xticks(np.arange(12),('1-Jan', '1-Feb', '1-Mar', '1-Apr', '1-May', '1-Jun', '1-Jul', '1-Aug', '1-Sep', '1-Oct', '1-Nov', '1-Dec'))
+    ax.yaxis.grid(True)        
 plots('Victoria')
 plt.savefig('output/Victoria.png')
 plt.show()
