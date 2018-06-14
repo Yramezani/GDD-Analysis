717b3d6a00a7c8f5830d323668211dd39e1d8345
Stephanie Gallant <smg561@mun.ca>
2018-06-14 10:50:36 -0230

    Updated

--- GDD_bokeh.py
+++ GDD_bokeh.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[4]:
+# In[28]:
 
 
 """ this function takes GDD values from a .csv file and creates a bokeh plot of the cumulative GDD value through the year"""
@@ -15,7 +15,7 @@ import sys
 import glob
 
 
-# In[10]:
+# In[45]:
 
 
 def make_bokehplot(filename): #input CSV file with GDD values
@@ -24,37 +24,28 @@ def make_bokehplot(filename): #input CSV file with GDD values
         df = pd.DataFrame(D)
         df.index
         accum_GDD = np.cumsum(df['GDD']) #sums the daily GDD values to make them accumulated
-
-   
-
+        #tup_GDD = zip(df,df.index) # for hovertools arg
     
         fig = figure(tools="pan,box_zoom,wheel_zoom,reset,save,hover,box_select", plot_width = 700, plot_height = 700, 
            title = "Accumulated GDD for Montreal in 2017", 
                 x_axis_label = 'Year (in days)', y_axis_label = 'Calculated GDD')
 
         
-        hover = HoverTool(
-            tooltips=[
-                ("Day of year", "$index"), 
-                ("GDD value", "$accum_GDD")
-                    ]
-                )
+        hover = HoverTool(tooltips=[
+                ("Index", "$index"),
+                ("(Day, Accum. GDD)", "($x, $y)")
+                        ])
 
         fig.circle(df.index, accum_GDD, size=9, color='green', alpha=0.5)
         
-        # save to html...
-        
-#         basename = os.path.basename(filename) # mydir/12345.csv -> 12345
-#         output = basename + '_bokeh.html'
-        
-        ### absolutely could not get the output to save in another folder, arugments needed commented out below
+                
+        ### absolutely could not get the output to save in another folder, arugments needed (but giving error) commented out below
         
         output_file('montreal_2017.html', title="Accum. GDD for Montreal") # mode='relative(-dev)', root_dir= notebooks/CMSC/Project/bokeh_plot
-        
         show(fig)
 
 
-# In[11]:
+# In[46]:
 
 
 make_bokehplot("./input/Montreal_2017*")
